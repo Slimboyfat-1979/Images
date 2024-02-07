1a32a97ae4eb17bee951dbde8f735e91155b1aac
unknown <slimboyfat1979@gmail.com>
2024-02-07 19:45:44 +1300

    Changed Port Number

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = 3000;
+const port = 443;
 
 const auth = 'b4zYtVzNzfAvpSbsSt2P9Ku9Hp4z2v2iVFVUGbS0MmY';
 
@@ -42,7 +42,7 @@ app.get("/", async (req, res) => {
             
         }
  
-        
+
     })
   
 })
